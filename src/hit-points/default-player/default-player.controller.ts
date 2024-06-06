169b58e27a205cdc98812dd46dbb8c122eb3d9ae
Erick Lombardo <lombardo79@gmail.com>
2024-06-06 11:55:36 -0600

    feat: add methods for handling TemporaryHitPoints feature

--- src/hit-points/default-player/default-player.controller.ts
+++ src/hit-points/default-player/default-player.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { DefaultPlayerService } from './default-player.service';
+import { DefaultPlayerService, PLAYER_ID } from './default-player.service';
 import { ApiOperation } from '@nestjs/swagger';
 import { UpdateHitPointsDto } from './dtos/update-hit-points.dto';
+import { AddTemporaryHitPointsDto } from './dtos/add-temporary-hit-points.dto';
 
 @Controller('default-player')
 export class DefaultPlayerController {
@@ -24,5 +25,18 @@ export class DefaultPlayerController {
   updateHitPoints(@Body() payload: UpdateHitPointsDto) {
     const { id, hitPoints } = payload;
     return this.defaultPlayerService.updateHitPoints(id, hitPoints);
+  }
+
+  @Post('add-temporary-hit-points')
+  @ApiOperation({ summary: 'Add temporary hit points to the default player' })
+  addTemporaryHitPoints(@Body() payload: AddTemporaryHitPointsDto) {
+    const { id, hitPoints } = payload;
+    return this.defaultPlayerService.addTemporaryHitPoints(id, hitPoints);
+  }
+
+  @Post('remove-first-temporary-hit-point')
+  @ApiOperation({ summary: 'Remove the first temporary hit point from the default player' })
+  removeFirstTemporaryHitPoint() {
+    return this.defaultPlayerService.removeFirstTemporaryHitPoint(PLAYER_ID);
   }
 }
--- src/hit-points/default-player/default-player.service.ts
+++ src/hit-points/default-player/default-player.service.ts
@@ -4,6 +4,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import * as player from '../../briv.json';
 
+export const PLAYER_ID = 'briv';
+
 @Injectable()
 export class DefaultPlayerService {
   constructor(
@@ -11,18 +13,33 @@ export class DefaultPlayerService {
   ) {}
 
   async createDefaultPlayer(): Promise<string> {
-    const createdPlayer = new this.playerModel(player);
+    const createdPlayer = new this.playerModel({ ...player, _id: PLAYER_ID });
     await createdPlayer.save();
     return createdPlayer.id;
   }
 
   getDefaultPlayer(): Promise<PlayerDocument> {
-    return this.playerModel.findOne().exec();
+    return this.playerModel.findOne({ _id: PLAYER_ID }).exec();
   }
 
   async updateHitPoints(id: string, hitPoints: number): Promise<void> {
     await this.playerModel
       .updateOne({ _id: new Types.ObjectId(id) }, { $inc: { hitPoints } })
+      .exec();
+  }
+
+  async addTemporaryHitPoints(id: string, hitPoints: number): Promise<void> {
+    await this.playerModel
+      .updateOne(
+        { _id: id },
+        { $push: { temporaryHitPoints: { $each: [hitPoints], $sort: -1 } } },
+      )
+      .exec();
+  }
+
+  async removeFirstTemporaryHitPoint(id: string): Promise<void> {
+    await this.playerModel
+      .updateOne({ _id: id }, { $pop: { temporaryHitPoints: -1 } })
       .exec();
   }
 }
