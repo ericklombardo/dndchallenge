5b3fb03e20aab95f29c5ef682eae7e081f457891
Erick Lombardo <lombardo79@gmail.com>
2024-06-06 09:44:12 -0600

    test: add unit testing for dealDamage feature

--- src/hit-points/hit-points.service.spec.ts
+++ src/hit-points/hit-points.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HitPointsService } from './hit-points.service';
+import { DefaultPlayerService } from './default-player/default-player.service';
+import * as player from '../briv.json';
 
 describe('HitPointsService', () => {
   let service: HitPointsService;
+  let mockDefaultPlayerService: {
+    getDefaultPlayer: jest.Mock;
+    updateHitPoints: jest.Mock;
+  };
 
   beforeEach(async () => {
+    mockDefaultPlayerService = {
+      getDefaultPlayer: jest.fn(),
+      updateHitPoints: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HitPointsService],
+      providers: [
+        HitPointsService,
+        { provide: DefaultPlayerService, useValue: mockDefaultPlayerService },
+      ],
     }).compile();
 
     service = module.get<HitPointsService>(HitPointsService);
@@ -14,5 +27,24 @@ describe('HitPointsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should deal piercing damage and update hit points', async () => {
+    const playerDocument = { ...player, id: '1' };
+    mockDefaultPlayerService.getDefaultPlayer.mockResolvedValue(playerDocument);
+    await service.dealDamage(5, 'piercing');
+    expect(mockDefaultPlayerService.getDefaultPlayer).toHaveBeenCalled();
+    expect(mockDefaultPlayerService.updateHitPoints).toHaveBeenCalledWith(
+      '1',
+      -5,
+    );
+  });
+
+  it("should deal fire damage and doesn't update hit points", async () => {
+    const playerDocument = { ...player, id: '1' };
+    mockDefaultPlayerService.getDefaultPlayer.mockResolvedValue(playerDocument);
+    await service.dealDamage(5, 'fire');
+    expect(mockDefaultPlayerService.getDefaultPlayer).toHaveBeenCalled();
+    expect(mockDefaultPlayerService.updateHitPoints).not.toHaveBeenCalled();
   });
 });
