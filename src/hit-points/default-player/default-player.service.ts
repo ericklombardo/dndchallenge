8c63fae1659f17df30a5f61c49ddd8091f5ddc1a
Erick Lombardo <lombardo79@gmail.com>
2024-06-06 06:13:41 -0600

    feat: add defaultPlayerService for creating/getting default player character

--- src/hit-points/default-player/default-player.service.ts
+++ src/hit-points/default-player/default-player.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Post } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Player } from '../schemas/player.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
