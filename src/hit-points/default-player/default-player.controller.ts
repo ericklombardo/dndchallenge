e0adfab8870e2822be1017f02a91c6f647122eb0
Erick Lombardo <lombardo79@gmail.com>
2024-06-06 13:44:39 -0600

    fix: remove un-used endpoint for defaultPlayerController

--- src/hit-points/default-player/default-player.controller.ts
+++ src/hit-points/default-player/default-player.controller.ts
@@ -1,8 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { DefaultPlayerService, PLAYER_ID } from './default-player.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { DefaultPlayerService } from './default-player.service';
 import { ApiOperation } from '@nestjs/swagger';
-import { UpdateHitPointsDto } from './dtos/update-hit-points.dto';
-import { AddTemporaryHitPointsDto } from './dtos/add-temporary-hit-points.dto';
 
 @Controller('default-player')
 export class DefaultPlayerController {
@@ -18,27 +16,5 @@ export class DefaultPlayerController {
   @ApiOperation({ summary: 'Get the default player' })
   get() {
     return this.defaultPlayerService.getDefaultPlayer();
-  }
-
-  @Post('update-hit-points')
-  @ApiOperation({ summary: 'Update the hit points of the default player' })
-  updateHitPoints(@Body() payload: UpdateHitPointsDto) {
-    const { id, hitPoints } = payload;
-    return this.defaultPlayerService.updateHitPoints(id, hitPoints);
-  }
-
-  @Post('add-temporary-hit-points')
-  @ApiOperation({ summary: 'Add temporary hit points to the default player' })
-  addTemporaryHitPoints(@Body() payload: AddTemporaryHitPointsDto) {
-    const { id, hitPoints } = payload;
-    return this.defaultPlayerService.addTemporaryHitPoints(id, hitPoints);
-  }
-
-  @Post('remove-first-temporary-hit-point')
-  @ApiOperation({
-    summary: 'Remove the first temporary hit point from the default player',
-  })
-  removeFirstTemporaryHitPoint() {
-    return this.defaultPlayerService.removeFirstTemporaryHitPoint(PLAYER_ID);
   }
 }
