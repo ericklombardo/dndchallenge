40c6ca3f439012d17a3e66dad2f5a1a99f760666
Erick Lombardo <lombardo79@gmail.com>
2024-06-06 09:42:35 -0600

    feat: add feature for deal damage considering resistances and immunities

--- src/hit-points/hit-points.service.ts
+++ src/hit-points/hit-points.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Player } from './schemas/player.schema';
-import { Model } from 'mongoose';
+import { DefaultPlayerService } from './default-player/default-player.service';
 
 @Injectable()
 export class HitPointsService {
-  constructor(
-    @InjectModel(Player.name) private readonly playerModel: Model<Player>,
-  ) {}
+  constructor(private readonly defaultPlayerService: DefaultPlayerService) {}
+
+  async dealDamage(damage: number, damageType: string) {
+    // Find the player
+    const player = await this.defaultPlayerService.getDefaultPlayer();
+    // Find the defense for the damage type
+    const defense = player.defenses.find(
+      (defense) => defense.type === damageType,
+    );
+    const noDamageDefenses = ['immunity', 'resistance'];
+    // No damage if the player has immunity or resistance
+    if (defense && noDamageDefenses.includes(defense.defense)) {
+      return;
+    }
+    // Update the player hit points decreasing it with the damage
+    await this.defaultPlayerService.updateHitPoints(player.id, -damage);
+  }
+
+  async heal(heal: number) {
+    const player = await this.defaultPlayerService.getDefaultPlayer();
+    await this.defaultPlayerService.updateHitPoints(player.id, heal);
+  }
+
+  // addTemporaryHitPoints(temporaryHitPoints: number) {}
 }
