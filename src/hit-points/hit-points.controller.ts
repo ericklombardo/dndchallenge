d1479b4a381c57dbdce77472eb0322dea791be7a
Erick Lombardo <lombardo79@gmail.com>
2024-06-06 06:14:28 -0600

    refactor: move method for creating default character to it's own service

--- src/hit-points/hit-points.controller.ts
+++ src/hit-points/hit-points.controller.ts
@@ -1,12 +1,7 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { HitPointsService } from './hit-points.service';
 
 @Controller('hit-points')
 export class HitPointsController {
   constructor(private readonly hitPointsService: HitPointsService) {}
-
-  @Post()
-  createDefaultPlayer() {
-    return this.hitPointsService.createDefaultPlayer();
-  }
 }
--- src/hit-points/hit-points.module.ts
+++ src/hit-points/hit-points.module.ts
@@ -3,12 +3,14 @@ import { HitPointsController } from './hit-points.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Player, PlayerSchema } from './schemas/player.schema';
 import { HitPointsService } from './hit-points.service';
+import { DefaultPlayerService } from './default-player/default-player.service';
+import { DefaultPlayerController } from './default-player/default-player.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Player.name, schema: PlayerSchema }]),
   ],
-  controllers: [HitPointsController],
-  providers: [HitPointsService],
+  controllers: [HitPointsController, DefaultPlayerController],
+  providers: [HitPointsService, DefaultPlayerService],
 })
 export class HitPointsModule {}
